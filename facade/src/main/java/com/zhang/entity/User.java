00587cf2f01254fdfc7f4030fa71aabe455d0e6d
zhang <54958512@qq.com>
2017-02-23 12:40:23 +0800

    第一天

--- facade/src/main/java/com/zhang/entity/User.java
+++ facade/src/main/java/com/zhang/entity/User.java
@@ -1,13 +1,20 @@
 package com.zhang.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
  * Created by zhang on 2017/2/22.
  */
+@Table(name = "t_user")
 public class User implements Serializable {
     private String name;
     private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     public String getName() {
